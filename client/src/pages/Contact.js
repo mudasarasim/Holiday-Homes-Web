c46b9af6364d1554146c2de3cb0578cc6593306b
Mudasar Shakil <117173890+mudasarasim@users.noreply.github.com>
2025-07-21 16:17:22 +0500

    lll

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -149,7 +149,7 @@ const ContactUs = () => {
 
             <div className="col-lg-6 mb-4">
               <div className="h-100 rounded-4 overflow-hidden shadow-lg">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3608.0648010095797!2d55.30321977538413!3d25.26840547766514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDE2JzA2LjMiTiA1NcKwMTgnMjAuOSJF!5e0!3m2!1sen!2sae!4v1753095820552!5m2!1sen!2sae" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3608.0648010095797!2d55.30321977538413!3d25.26840547766514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDE2JzA2LjMiTiA1NcKwMTgnMjAuOSJF!5e0!3m2!1sen!2sae!4v1753095820552!5m2!1sen!2sae" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
               </div>
             </div>
           </div>
